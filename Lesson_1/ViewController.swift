d7d1601089e04b2479432d27aeea603feb5183e0
Denis Skokov <13denilson@gmail.com>
2020-03-23 20:00:22 +0300

    Initial Commit

---
+++ Lesson_1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lesson_1
+//
+//  Created by D S on 23/03/2020.
+//  Copyright © 2020 Denis Skokov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

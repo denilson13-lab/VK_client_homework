dc2e32b8dbe811a2a8e80dd84f5e20951a56c92a
Denis Skokov <13denilson@gmail.com>
2020-03-26 18:34:39 +0300

    Урок 1

--- Lesson_1/ViewController.swift
+++ Lesson_1/ViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollBottomConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var loginInput: UITextField!
+    @IBOutlet weak var passwordInput: UITextField!
+    
+    @IBAction func enterButton() {
+        let login = loginInput.text!
+        let password = passwordInput.text!
+        if login == "Denis" && password == "123" {
+            print("Успешная авторизация")
+        } else {
+            print("неуспешная авторизация")
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWasShown(notification:)),
+            name: UIResponder.keyboardDidShowNotification,
+            object: nil)
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillBeHidden(notification:)),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil)
+        }
+
+    @objc func keyboardWasShown(notification: Notification) {
+        let userInfo = (notification as NSNotification).userInfo as! [String: Any]
+        let frame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
+        scrollBottomConstraint.constant = frame.height
     }
-
-
+    
+    @objc func keyboardWillBeHidden(notification: Notification) {
+        scrollBottomConstraint.constant = 0
+    }
 }
 

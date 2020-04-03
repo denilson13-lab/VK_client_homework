33b4e1a968a2a8332328630d5167dc2e95b6c12a
Denis Skokov <13denilson@gmail.com>
2020-04-04 00:15:20 +0300

    Lesson 3 completed

---
+++ Lesson_1/FriendsCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  FriendsCollectionViewCell.swift
+//  Lesson_1
+//
+//  Created by Denis on 01.04.2020.
+//  Copyright © 2020 Denis Skokov. All rights reserved.
+//
+
+import UIKit
+
+class FriendsCollectionViewCell: UICollectionViewCell {
+    
+}

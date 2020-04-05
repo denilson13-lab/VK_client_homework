7104d36d228b04b17cdd38c45effd0a8e5d0a806
Denis Skokov <13denilson@gmail.com>
2020-04-05 22:45:57 +0300

    Lesson 4 completed

---
+++ Lesson_1/HeartButton.swift
@@ -0,0 +1,40 @@
+//
+//  Heart.swift
+//  Lesson_1
+//
+//  Created by Denis on 05.04.2020.
+//  Copyright © 2020 Denis Skokov. All rights reserved.
+//
+
+import UIKit
+
+class Heart: UIView {
+
+    var heart = UIButton()
+    var heartCount = UILabel()
+   
+    override func awakeFromNib() {
+         heart = UIButton(type: .roundedRect)
+         heart.frame = CGRect(x: 5, y: 5, width: 20, height: 20)
+         heart.addTarget(self, action: #selector(buttonIsTapped), for: .touchUpInside)
+         heart.setBackgroundImage(UIImage(named: "heartEmpty"), for: .normal)
+         self.addSubview(heart)
+    
+    
+         let labelFrame = CGRect(x: 27, y: 2, width: 30, height: 30)
+         heartCount.frame = labelFrame
+         heartCount.text = "0"
+         self.addSubview(heartCount)
+    }
+    
+    @objc func buttonIsTapped(sender: UIButton) {
+        if heartCount.text == "0" {
+            heartCount.text = "1"
+            heartCount.textColor = .red
+            heart.setBackgroundImage(UIImage(named: "heartFilled"), for: .normal)}
+        else {
+            heartCount.text = "0"
+            heartCount.textColor = .black
+            heart.setBackgroundImage(UIImage(named: "heartEmpty"), for: .normal)}
+    }
+}
---
+++ Lesson_1/Models/Users.swift
@@ -0,0 +1,9 @@
+//
+//  Users.swift
+//  Lesson_1
+//
+//  Created by Denis on 04.04.2020.
+//  Copyright © 2020 Denis Skokov. All rights reserved.
+//
+
+import Foundation
